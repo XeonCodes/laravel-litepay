ac55a5f5e60a7c8cb9ce05cbaa2677967ff344eb
Oludowole Olumide <xeonncodes@gmail.com>
2025-05-12 13:24:52 +0100

    scsc

--- project/app/Http/Controllers/UserController.php
+++ project/app/Http/Controllers/UserController.php
@@ -6,34 +6,52 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     // Get user balance
-    public function getUserBalance(Request $request){
+    public function register(Request $request){
         try {
 
-            $userId = $request->input("user_id");
-            $user = User::where("id", $userId)->first();
+            // Validate the request
+            $validate = Validator::make($request->all(), [
+                "first_name" => "required|string|max:15",
+                "last_name" => "required|string|max:15",
+                "email" => "required|email|unique:users,email",
+                "username" => "required|unique:users,username|min3|max:15",
+                "phone_number" => "required|unique:users,phone_number|regex:/^0[0-9]{10}$/",
+                "password" => "required|string|min:6"
+            ]);
 
-            if(!$user){
+            
+            // Check if validation fails
+            if($validate->fails()){
                 return response()->json([
-                    "status" => 404,
-                    "message" => "Bad request",
-                ], 400);
+                    "status" => 422,
+                    "message" => $validate->errors()->first()
+                ], 422);
             }
 
+            // Success response
             return response()->json([
                 "status" => 200,
-                "message" => $user->balance,
+                "message" => "User registered successfully",
+                "data" => [
+                    "first_name" => $request->first_name,
+                    "last_name" => $request->last_name,
+                    "email" => $request->email,
+                    "username" => $request->username,
+                    "phone_number" => $request->phone_number
+                ]
             ], 200);
+
         } catch (Exception $error) {
-            // Log error to the server
-            Log::error($error->getMessage());
+            Log::error("Error in UserController@register: " . $error->getMessage());
             return response()->json([
-                "status" => 500,
-                "message" => "Something went wrong",
-            ], 500);
+                    "status" => 500,
+                    "message" => $error->getMessage()
+                ], 500);
         }
     }
 }
